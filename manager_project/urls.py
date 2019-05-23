528a4261e410d4f6b375e9f3bd8f3c2c09fc0588
ayu <goldayushi@gmail.com>
2019-05-23 22:14:53 +0900

    feat: Add url and views.

--- manager_project/urls.py
+++ manager_project/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import manager.views as manager_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('worker_list/', manager_view.WorkerListView.as_view()),
+    path('', include('blog.urls')),
 ]
